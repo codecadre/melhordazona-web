480ae88b09a965e7418f19e7df890523838deee2
Flávio Sousa <root@fjsousa.com>
2022-05-20 19:02:53 +0100

    solo page styling

--- src/main/bb_passrates/backend/pages/school.clj
+++ src/main/bb_passrates/backend/pages/school.clj
@@ -29,16 +29,24 @@
        [:div.solo-school
         [:div.container
          [:div.row
+          [:p.label "Escola de Condução"]
           [:h4.name (:name imt-profile)]
-          [:a {:href (:concelho-href imt-profile)}
-           (format "Mais escolas no concelho de %s." (:concelho imt-profile))]
+          [:p [:a {:href (:concelho-href imt-profile)}
+            (format "Ver Mais escolas no concelho de %s." (:concelho imt-profile))]]
           [:div.row
            [:div.six.columns
-            [:p.address-label "Morada"]
-            [:p.address (:address imt-profile)]
-            [:p.license-label "Licensa IMT"]
-            [:p.lices (:nec imt-profile)]
-            [:p "Fonte dos Dados:" [:a {:href (:imt-href imt-profile)} "IMT"]]]
+            [:p.label "Licensa IMT"]
+            [:p.field (:nec imt-profile)]
+            [:p.label "Morada"]
+            [:p.field (:address imt-profile)]
+            [:p.label "Concelho"]
+            [:p.field "Lisboa"]
+            [:p.label "Codigo Postal"]
+            [:p.field (:cp7 imt-profile)]
+            [:p.source "Fonte dos Dados:" [:a {:href (:imt-href imt-profile)} "Escola no site do IMT"]]
+            [:p.label "coordenadas (Aproximadas)"]
+            [:p.field lat " " long]
+            [:p.source "Fonte dos Dados:" [:a {:href (:imt-href imt-profile)} "ESRI"]]]
            [:div.six.columns
             [:div#map-solo {:lat lat :long long}]]]
           [:div.row
@@ -47,6 +55,7 @@
              (svg/pop-up-svg (svg/parse-d-smart rates :d year-selector) (count year-selector))]]
            [:div.six.columns
             [:div.theory
-             (svg/pop-up-svg (svg/parse-d-smart rates nil year-selector) (count year-selector))]]]
+             (svg/pop-up-svg (svg/parse-d-smart rates nil year-selector) (count year-selector))]
+            [:p.source "Fonte dos Dados:" [:a {:href (:imt-href imt-profile)} "ESRI"]]]]
           [:div.row.description
-           [:p.label "This is a label that explain the charts above. " [:span.driving "This is for driving"] " While, " [:span.theory "This other is for theory" " Everything looks amazing."] ]]]]]])]))
+           [:p "This is a label that explain the charts above. " [:span.driving "This is for driving"] " While, " [:span.theory "This other is for theory" " Everything looks amazing."] ]]]]]])]))
