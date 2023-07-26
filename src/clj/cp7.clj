d7931e6a54e2a659ec2d491656b8d8dbcc81567f
Flávio Sousa <root@fjsousa.com>
2023-07-26 11:24:34 +0100

    new batch of cp7

--- src/clj/cp7.clj
+++ src/clj/cp7.clj
@@ -11,7 +11,9 @@
 
 (def d (util/imt-profiles))
 
-(def cp7
+;; Non nil post codes in the format xxxx-xxx
+(def
+  cp7
   (->> d
        (map :cp7)
        (filter #(not (nil? %)))
@@ -19,28 +21,7 @@
        set
        vec))
 
-
-(def id->address
-  (reduce (fn [acc {:keys [id address]}]
-            (assoc acc id address)) {} d))
-
-
-;; (defn cp7->request-obj []
-;;   (->> cp7
-;;        (map-indexed (fn [idx v]
-;;                       {:attributes {:objectid idx
-;;                                     :address v}}))))
-
-
-;; (defn s [] (json/generate-string {:records cp7->request-obj}))
-
-#_(def r (:body (curl/post "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/geocodeAddresses"
-                         {:query-params [["countryCode" "PRT"]
-                                         ["sourceCountry" "PRT"]
-                                   ["token" esri-token]]
-                    :form-params [["f" "json"]
-                                  ["addresses" s]]})))
-
+;;BATCH API - not used atm
 (defn single-batch
   "single request to batch api - testing purposes"
   [s]
@@ -69,7 +50,7 @@
                                               (if postal
                                                 ["postal" postal]
                                                 ["address" address]))}))
-       json/parse-string
+       (json/parse-string true)
        #_(get "locations"))
     (catch Exception e
       (do
@@ -77,93 +58,40 @@
         (Thread/sleep 5000)
         (single-non-stored a)))))
 
-#_(comment
-  (let [current (last foo)
-        id (:id current)
-        address (:address current)
-        r (single-non-stored {:address address}
-                             #_{:postal "4615-301"})
-        candidates (get r "candidates")
-        address-c (-> candidates first (get "address"))
-        postal-c  (-> candidates first (get "attributes") (get "Postal"))
-        x (-> candidates first (get "location") (get "x"))
-        y (-> candidates first (get "location") (get "y"))
-        score (-> candidates first (get "score"))]
-    {:id id
-     :address address
-     :address-c address-c
-     :postal-c postal-c
-     :x x
-     :y y
-     :score score
-     :c (count candidates)}))
-
-
 (comment
   (single-non-stored {:postal "4200-163"})
 
   (let [a "Estrada Exterior da Circunvalação, 8154 4200-163 PORTO"]
     (single-non-stored {:address a})))
 
-;;TODO use recur ..
-(defn non-stored-address [k address-map]
-  (let [a (atom [])]
-    (doseq [[id address] address-map]
-      (let [r (single-non-stored (hash-map k address))
-            candidates (get r "candidates")
-            address-c (-> candidates first (get "address"))
-            postal-c  (-> candidates first (get "attributes") (get "Postal"))
-            x (-> candidates first (get "location") (get "x"))
-            y (-> candidates first (get "location") (get "y"))
-            score (-> candidates first (get "score"))]
-        (swap! a conj  {:id id
-                        :address address
-                        :address-c address-c
-                        :postal-c postal-c
-                        :x x
-                        :y y
-                        :score score
-                        :c (count candidates)})))
-    (sort #(compare (:score %2) (:score %1)) @a)))
+;;TODO check local first, then pull from api if necessary
+(defn non-stored-postal
+  [cp7s]
+  (->>
+   (loop [cp7-set cp7s
+          results []]
+     (let [cp7 (first cp7-set)
+           {:keys [candidates]} (single-non-stored {:postal cp7})
+           postal-c  (-> candidates first :attributes :Postal)
+           {:keys [x y]} (-> candidates first :location)
+           score (-> candidates first :score)
+           results (conj results
+                         {:cp7 cp7
+                          :postal-c postal-c
+                          :x x
+                          :y y
+                          :score score
+                          :c (count candidates)})]
+       (if (empty? (rest cp7-set))
+         results
+         (recur (rest cp7-set) results))))
+   (sort-by :cp7)))
 
-#_(non-stored-address :address (into {} (take 2 id->address)))
-
-#_(let [d (doall (non-stored-address :address id->address))
-      f "recepies/address-geocode"
-      ks '(:address :address-c :postal-c :c :score :x :y :id)]
-  (spit (str f ".txt") (with-out-str (pprint/print-table ks d)))
-  (spit (str f ".edn") (with-out-str (pprint/pprint d))))
-
-;;TODO use recur ..
-(defn non-stored-postal [cp7s]
-  (let [a (atom [])]
-    (doseq [cp7 cp7s]
-      (let [r (single-non-stored {:postal cp7})
-            candidates (get r "candidates")
-            postal-c  (-> candidates first (get "attributes") (get "Postal"))
-            x (-> candidates first (get "location") (get "x"))
-            y (-> candidates first (get "location") (get "y"))
-            score (-> candidates first (get "score"))]
-        (swap! a conj  {:cp7 cp7
-                        :postal-c postal-c
-                        :x x
-                        :y y
-                        :score score
-                        :c (count candidates)})))
-    (sort #(compare (:score %2) (:score %1)) @a)))
-
-
-(defn -main []
-  (let [d (doall (non-stored-postal (take 3 cp7)))
+(defn -main [& _args]
+  (let [[n & _] _args
+        n  (when n (edn/read-string n))
+        d (doall (non-stored-postal (if n (take n cp7)
+                                        cp7)))
         f "resources/data/cp7"]
     (spit (str f ".txt") (with-out-str (pprint/print-table '(:cp7 :postal-c :c :score :x :y) d)))
     (spit (str f ".edn") (with-out-str (pprint/pprint d)))))
-
-
-
-(let [d (->>
-         (edn/read-string (slurp "resources/data/cp7.edn"))
-         (sort-by :cp7))
-      f "resources/data/cp7"]
-    (spit (str f ".txt") (with-out-str (pprint/print-table '(:cp7 :postal-c :c :score :x :y) d)))
-    (spit (str f ".edn") (with-out-str (pprint/pprint d))))
